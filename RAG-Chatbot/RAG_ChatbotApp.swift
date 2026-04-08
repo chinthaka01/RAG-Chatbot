1fca0b9d4511b9e6b2724aef1113e1f58d118269
chinthaka01 <chinthaka01@gmail.com>
2026-04-07 20:49:00 -0400

    Initial Commit

---
+++ RAG-Chatbot/RAG_ChatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAG_ChatbotApp.swift
+//  RAG-Chatbot
+//
+//  Created by Chinthaka Perera on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct RAG_ChatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RAG-ChatbotTests/RAG_ChatbotTests.swift
@@ -0,0 +1,17 @@
+//
+//  RAG_ChatbotTests.swift
+//  RAG-ChatbotTests
+//
+//  Created by Chinthaka Perera on 4/7/26.
+//
+
+import Testing
+@testable import RAG_Chatbot
+
+struct RAG_ChatbotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
